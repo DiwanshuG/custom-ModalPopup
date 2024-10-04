4575f52d618c1c665828bd5459bd5c3b7ab06cc4
Diwanshu Gangwar <118631453+DiwanshuG@users.noreply.github.com>
2024-10-05 03:24:20 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import ModalTest from "./components/custom-model-popup/modal-test";
+function App() {
+  return (
+    <div className="App">
+     {/* Custom Modal Popup  */}
+      <ModalTest />
+    </div>
+  );
+}
+export default App;
